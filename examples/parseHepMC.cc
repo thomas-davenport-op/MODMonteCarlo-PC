abe0ec622710d1e7aebbb6d082dba463d757fb41
Aashish Tripathee <aashisht@mit.edu>
2015-10-20 17:45:06 -0400

    More stuff

--- examples/parseHepMC.cc
+++ examples/parseHepMC.cc
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <iomanip>
+#include <sstream>
 
 #include "HepMC/IO_GenEvent.h"
 
@@ -9,38 +10,54 @@ using namespace HepMC;
 
 int main(int argc, char* argv[]) {
 
-  ifstream file_stream_to_parse(argv[1]);
-  istream & parse_stream = file_stream_to_parse;
+  string input_file_name = argv[1];
+  string output_file_name = argv[2];
 
+  ofstream output_file(output_file_name, ios::out);
 
-
-  // IO_GenEvent gen_event(file_stream_to_parse);
-  // int icount=0;
-  // HepMC::GenEvent* evt = gen_event.read_next_event();
-
-  HepMC::IO_GenEvent ascii_out("2.dat", std::ios::out);
-
-  HepMC::IO_GenEvent ascii_in("output.dat", std::ios::in);
+  HepMC::IO_GenEvent ascii_in(input_file_name, std::ios::in);
+  stringstream output_stream;
   
   int icount = 0;
-  HepMC::GenEvent* evt = ascii_in.read_next_event();
+  HepMC::GenEvent* event = ascii_in.read_next_event();
 
-
-  cout << "Strating things!" << endl;
-  
-  while ( evt ) {
+  while (event) {
   
     icount++;
   
-    std::cout << "Processing Event Number " << icount << " its # " << evt->event_number()  << std::endl;
+    std::cout << "Processing Event Number " << icount << std::endl;
     
-    // write the event out to the ascii file
-    ascii_out << evt;
-    delete evt;
-    ascii_in >> evt;
+    cout << "Number of particles: " << event->particles_size() << endl;
+    cout << "Number of vertices: " << event->vertices_size() << endl;
+
+    GenEvent::particle_const_iterator particle = event->particles_begin();
+
+    HepMC::GenEvent::particle_const_iterator it; 
+
+    output_stream << "BeginEvent Version 4 Pythia_8212 TruthParticles" << endl;
+    
+    output_stream << "#TPFC" << "              px              py              pz          energy   pdgId" << endl;  
+    
+    for ( it = event->particles_begin(); it != event->particles_end(); it++ ) {
+      output_stream << " TPFC"
+        << setw(16) << fixed << setprecision(8) << ( * it)->momentum().px()
+        << setw(16) << fixed << setprecision(8) << ( * it)->momentum().py()
+        << setw(16) << fixed << setprecision(8) << ( * it)->momentum().pz()
+        << setw(16) << fixed << setprecision(8) << ( * it)->momentum().e()
+        << setw(8) << noshowpos << ( * it)->pdg_id()
+        << endl;
+    }
+    
+    output_stream << "EndEvent" << endl;
+    
+    delete event;
+    ascii_in >> event;
   }
 
-  std::cout << icount << " events found. Finished." << std::endl;
+
+  output_file << output_stream.rdbuf();
+
+  std::cout << icount << " events found. And I'm done." << std::endl;
   
 
   return 0;
