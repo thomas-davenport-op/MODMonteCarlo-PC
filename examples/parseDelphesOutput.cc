29e84137d0850a754c928eeb433134b4225547a6
Aashish Tripathee <aashisht@mit.edu>
2015-10-28 15:26:10 -0400

    parseDelphes

--- examples/parseDelphesOutput.cc
+++ examples/parseDelphesOutput.cc
@@ -1,86 +1,55 @@
 /*
- *  Delphes: a framework for fast simulation of a generic collider experiment
- *  Copyright (C) 2012-2014  Universite catholique de Louvain (UCL), Belgium
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+Simple macro showing how to access branches from the delphes output root file,
+loop over events, and plot simple quantities such as the jet pt and the di-electron invariant
+mass.
 
+root -l examples/parseDelphesOutput.C'("delphes_output.root")'
+*/
 
-#include <iostream>
-#include <utility>
-#include <vector>
-
-#include "TROOT.h"
-#include "TSystem.h"
-#include "TApplication.h"
-
-#include "TString.h"
-
-#include "TH2.h"
-#include "THStack.h"
-#include "TLegend.h"
-#include "TPaveText.h"
-#include "TClonesArray.h"
-#include "TLorentzVector.h"
-
+#ifdef __CLING__
+R__LOAD_LIBRARY(libDelphes)
 #include "classes/DelphesClasses.h"
-
-#include "ExRootAnalysis/ExRootTreeReader.h"
-#include "ExRootAnalysis/ExRootTreeWriter.h"
-#include "ExRootAnalysis/ExRootTreeBranch.h"
-#include "ExRootAnalysis/ExRootResult.h"
-#include "ExRootAnalysis/ExRootUtilities.h"
-
-using namespace std;
+#include "external/ExRootAnalysis/ExRootTreeReader.h"
+#endif
 
 //------------------------------------------------------------------------------
 
-// Here you can put your analysis macro
+void parseDelphesOutput(const char * inputFile, const char * outputFileName) {
 
-// #include "Example1.C"
 
-//------------------------------------------------------------------------------
+  ofstream output_file(outputFileName, ios::out);
 
-int main(int argc, char *argv[])
-{
-  // char *appName = "Example1";
 
-  // if(argc != 2)
-  // {
-    // cout << " Usage: " << appName << " input_file" << endl;
-    // cout << " input_file - input file in ROOT format ('Delphes' tree)," << endl;
-    // return 1;
-  // }
+  gSystem->Load("libDelphes");
 
-  // gROOT->SetBatch();
+  // Create chain of root trees
+  TChain chain("Delphes");
+  chain.Add(inputFile);
 
-  // int appargc = 1;
-  // char *appargv[] = {appName};
-  // TApplication app(appName, &appargc, appargv);
+  // Create object of class ExRootTreeReader
+  ExRootTreeReader *treeReader = new ExRootTreeReader(&chain);
+  Long64_t numberOfEntries = treeReader->GetEntries();
 
-  // TString inputFile(argv[1]);
+  // Get pointers to branches used in this analysis
+  TClonesArray *branchJet = treeReader->UseBranch("Jet");
 
-//------------------------------------------------------------------------------
+  TClonesArray *branchParticle = treeReader->UseBranch("Particle");
 
-// Here you call your macro's main function
+  // Loop over all events
+  for (Int_t entry = 0; entry < numberOfEntries; ++entry) {
+    // Load selected branches with data from specified event
+    treeReader->ReadEntry(entry);
 
-  // Example1(inputFile);
+    if (branchJet->GetEntries() > 0) {
+      Jet *jet = (Jet*) branchJet->At(0);
 
-//------------------------------------------------------------------------------
+      cout << "Jet pt: " << jet->PT << endl;
 
-  return 0;
+      output_file << jet->PT << endl;
+
+    }
+    
+  }
 
 }
-
 
