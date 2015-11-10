b919f62209c282fafd45542a9fe596e6c76fb5c7
Aashish Tripathee <aashisht@mit.edu>
2015-11-10 10:56:54 -0500

    PFCs

--- examples/parseDelphesOutput.cc
+++ examples/parseDelphesOutput.cc
@@ -48,13 +48,21 @@ void parseDelphesOutput(const char * inputFile, const char * outputFileName) {
   // Get pointers to branches used in this analysis.
   TClonesArray *branch_jet = tree_reader->UseBranch("Jet");
   TClonesArray *branch_particle = tree_reader->UseBranch("Particle");  // This is for Truth Particles.
+  
+  TClonesArray *branch_genparticle = tree_reader->UseBranch("GenParticle"); 
 
-  TClonesArray *branch_eflow_track = tree_reader->UseBranch("EFlowTrack");
-  TClonesArray *branch_photon = tree_reader->UseBranch("EFlowPhoton");
-  TClonesArray *branch_neutral_hadron = tree_reader->UseBranch("EFlowNeutralHadron");
+  TClonesArray *branch_electron = tree_reader->UseBranch("Electron");
+  TClonesArray *branch_photon = tree_reader->UseBranch("Photon");
+  TClonesArray *branch_muon = tree_reader->UseBranch("Muon");
 
   TClonesArray *branch_track = tree_reader->UseBranch("Track");
   TClonesArray *branch_tower = tree_reader->UseBranch("Tower");
+
+  TClonesArray *branch_eflow_track = tree_reader->UseBranch("EFlowTrack");
+  TClonesArray *branch_eflow_photon = tree_reader->UseBranch("EFlowPhoton");
+  TClonesArray *branch_eflow_neutral_hadron = tree_reader->UseBranch("EFlowNeutralHadron");
+
+  
 
 
 
@@ -73,9 +81,10 @@ void parseDelphesOutput(const char * inputFile, const char * outputFileName) {
     // Output all jets.
     output_stream << "#  MAK5" << "              px              py              pz          energy" << endl;
 
+
     for (unsigned i = 0; i < branch_jet->GetEntries(); i++) {
       Jet *jet = (Jet*) branch_jet->At(i);
-      
+
       // Because 'jet' does not directly provide (px, py, pz, E), write out (pT, eta, phi, m) first and then use that to calculate (px, py, pz, E).
       fastjet::PseudoJet pseudojet = fastjet::PseudoJet(0., 0., 0., 0.);
       pseudojet.reset_PtYPhiM(jet->PT, jet->Eta, jet->Phi, jet->Mass);
@@ -86,7 +95,9 @@ void parseDelphesOutput(const char * inputFile, const char * outputFileName) {
         << setw(16) << fixed << setprecision(8) << pseudojet.pz()
         << setw(16) << fixed << setprecision(8) << pseudojet.E()
         << endl;
+
     }
+
 
     // Output all truth particles.
     output_stream << "# TRUTH" << "              px              py              pz          energy   pdgId" << endl;  
@@ -117,20 +128,107 @@ void parseDelphesOutput(const char * inputFile, const char * outputFileName) {
     
     double pfc_total_energy = 0.0;
 
-    for (unsigned i = 0; i < branch_eflow_track->GetEntriesFast(); i++) {
-      GenParticle * track = (GenParticle*) branch_eflow_track->At(i);
+    // Loop over all electrons in event
+    for(unsigned i = 0; i < branch_electron->GetEntriesFast(); ++i) {
+      Electron * electron = (Electron*) branch_electron->At(i);
 
-      if (track->Status == 1) {
-        output_stream << "   MPFC"
-        << setw(16) << fixed << setprecision(8) << track->Px
-        << setw(16) << fixed << setprecision(8) << track->Py
-        << setw(16) << fixed << setprecision(8) << track->Pz
-        << setw(16) << fixed << setprecision(8) << track->E
+      TLorentzVector four_vector = electron->P4();
+      
+      pfc_total_energy += four_vector.E();
+    }
+
+    // Loop over all photons in event
+    for(unsigned i = 0; i < branch_photon->GetEntriesFast(); ++i){
+      Photon * photon = (Photon*) branch_photon->At(i);
+
+      // skip photons with references to multiple particles
+      if(photon->Particles.GetEntriesFast() != 1) continue;
+
+      TLorentzVector four_vector = photon->P4();
+      
+      pfc_total_energy += four_vector.E(); 
+    }
+
+    // Loop over all muons in event
+    for(unsigned i = 0; i < branch_muon->GetEntriesFast(); ++i)
+    {
+      Muon * muon = (Muon*) branch_muon->At(i);
+      TLorentzVector four_vector = muon->P4();
+      
+      pfc_total_energy += four_vector.E();
+    }
+
+
+    // Loop over all tracks in event
+    for(unsigned i = 0; i < branch_track->GetEntriesFast(); ++i) {
+      Track * track = (Track*) branch_track->At(i);
+      TLorentzVector four_vector = track->P4();
+      
+      output_stream << "   MPFC"
+        << setw(16) << fixed << setprecision(8) << four_vector.Px()
+        << setw(16) << fixed << setprecision(8) << four_vector.Py()
+        << setw(16) << fixed << setprecision(8) << four_vector.Pz()
+        << setw(16) << fixed << setprecision(8) << four_vector.E()
         << setw(8) << noshowpos << track->PID
         << endl;
 
-        pfc_total_energy += track->E;
-      }
+        pfc_total_energy += four_vector.E();
+    }
+
+    // Loop over all EFlow tracks
+    for (unsigned i = 0; i < branch_eflow_track->GetEntriesFast(); ++i) {
+      Track * track = (Track*) branch_eflow_track->At(i);
+      TLorentzVector four_vector = track->P4();
+
+      pfc_total_energy += four_vector.E();
+    }
+
+
+    // Loop over all GenParticles
+    for (unsigned i = 0; i < branch_genparticle->GetEntriesFast(); ++i) {
+      GenParticle * gen_particle = (GenParticle*) branch_genparticle->At(i);
+      // TLorentzVector four_vector = gen_particle->P4();
+
+      // pfc_total_energy += four_vector.E();
+    }
+
+    // Loop over all EFlow Photons
+    for (unsigned i = 0; i < branch_eflow_photon->GetEntriesFast(); ++i) {
+      Photon * photon = (Photon*) branch_eflow_photon->At(i);
+      TLorentzVector four_vector = photon->P4();
+
+      pfc_total_energy += four_vector.E();
+    }
+
+    // Loop over all EFlow Hadrons
+    for (unsigned i = 0; i < branch_eflow_neutral_hadron->GetEntriesFast(); ++i) {
+      GenParticle * neutral_hadron = (GenParticle*) branch_eflow_neutral_hadron->At(i);
+      TLorentzVector four_vector = neutral_hadron->P4();
+
+      pfc_total_energy += four_vector.E();
+    }
+
+
+
+    /*
+    for (unsigned i = 0; i < branch_eflow_track->GetEntriesFast(); i++) {
+      Track * track = (Track * ) branch_eflow_track->At(i);
+      
+      // Because 'Track' does not directly provide (px, py, pz, E), write out (pT, eta, phi, m) first and then use that to calculate (px, py, pz, E).
+      fastjet::PseudoJet pseudojet = fastjet::PseudoJet(0., 0., 0., 0.);
+      pseudojet.reset_PtYPhiM(track->PT, track->Eta, track->Phi, 0.);
+
+      // if (track->Status == 1) {
+        output_stream << "   MPFC"
+        << setw(16) << fixed << setprecision(8) << pseudojet.px()
+        << setw(16) << fixed << setprecision(8) << pseudojet.py()
+        << setw(16) << fixed << setprecision(8) << pseudojet.pz()
+        << setw(16) << fixed << setprecision(8) << pseudojet.E()
+        << setw(8) << noshowpos << track->PID
+        << endl;
+
+        pfc_total_energy += pseudojet.E();
+      // }
     }
 
     for (unsigned i = 0; i < branch_photon->GetEntriesFast(); i++) {
@@ -164,6 +262,7 @@ void parseDelphesOutput(const char * inputFile, const char * outputFileName) {
         pfc_total_energy += neutral_hadron->E;
       }
     }
+    */
 
     cout << "PFC Total Energy: " << pfc_total_energy << endl;
 
