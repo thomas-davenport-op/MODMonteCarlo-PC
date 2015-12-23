4a691beb345e70e250a22fc20fb9a3df08d66193
Aashish Tripathee <aashisht@mit.edu>
2015-12-24 02:46:39 +0545

    Things

--- src/generateEvents.cc
+++ src/generateEvents.cc
@@ -21,12 +21,6 @@ int main(int argc, char* argv[]) {
 
   // Interface for conversion from Pythia8::Event to HepMC event.
   HepMC::Pythia8ToHepMC ToHepMC;
-
-  // Shorthand for the event record in pythia.
-  Event& event = pythia.event;
-  // Write out zg information.
-  ofstream output_file("/home/aashish/zg_from_pythia.dat", ios::out);
-  stringstream output;
   
   // Read the card file.
   pythia.readFile(argv[1]);
@@ -53,28 +47,6 @@ int main(int argc, char* argv[]) {
 
     }
 
-
-
-    // Loop over particles.
-    vector<fastjet::PseudoJet> all_particles;
-    for (int i = 0; i < event.size(); ++i) if (event[i].isFinal()) {
-      all_particles.push_back(fastjet::PseudoJet(event[i].px(), event[i].py(), event[i].pz(), event[i].e()));
-    }
-
-    // Cluster all particles.
-    JetDefinition jet_def(antikt_algorithm, 0.5);
-    ClusterSequence cs(all_particles, jet_def);
-    vector<PseudoJet> fastjet_jets = sorted_by_pt(cs.inclusive_jets(0.0));
-
-    // Apply SoftDrop on the hardest jet.
-    SoftDrop soft_drop_1(0.0, 0.1);
-    PseudoJet soft_drop_jet_1 = soft_drop_1(fastjet_jets[0]);
-    double zg_1 = soft_drop_jet_1.structure_of<SoftDrop>().symmetry();
-
-    // cout << zg_1 << endl;
-    output << fastjet_jets[0].pt() << "  " << zg_1 << endl;
-
-
     // Construct new empty HepMC event and fill it.
     // Units will be as chosen for HepMC build, but can be changed
     // by arguments, e.g. GenEvt( HepMC::Units::GEV, HepMC::Units::MM)
@@ -94,7 +66,7 @@ int main(int argc, char* argv[]) {
 
   // ofstream output_file("output.dat", ios::out | ios::app);
   
-  output_file << output.rdbuf();
+
 
   return 0;
 }
--- utilities/run.py
+++ utilities/run.py
@@ -11,11 +11,11 @@ def run_analyzer():
 		call(['rm', 'data/pythia_truth.hepmc', 'data/herwig_truth.hepmc', 'data/sherpa_truth.hepmc'])
 		call(['rm', 'data/pythia_reco.hepmc', 'data/herwig_reco.hepmc', 'data/sherpa_reco.hepmc'])
 
-		# call(['rm', 'data/pythia_truth.mod', 'data/herwig_truth.mod', 'data/sherpa_truth.mod'])
-		# call(['rm', 'data/pythia_reco.mod', 'data/herwig_reco.mod', 'data/sherpa_reco.mod'])
-		# call(['rm', 'herwig_run.log', 'herwig_run.out', 'herwig_run.run', 'herwig_run.tex'])
+		call(['rm', 'data/pythia_truth.mod', 'data/herwig_truth.mod', 'data/sherpa_truth.mod'])
+		call(['rm', 'data/pythia_reco.mod', 'data/herwig_reco.mod', 'data/sherpa_reco.mod'])
+		call(['rm', 'herwig_run.log', 'herwig_run.out', 'herwig_run.run', 'herwig_run.tex'])
 		
-		# call(['rm', 'MIG_P+P+_7000_NNPDF_2.db', 'MPI_Cross_Sections.dat', 'Results.db', 'Results.db.bak', 'Sherpa_References.tex'])
+		call(['rm', 'MIG_P+P+_7000_NNPDF_2.db', 'MPI_Cross_Sections.dat', 'Results.db', 'Results.db.bak', 'Sherpa_References.tex'])
 
 	def cleanup():
 		call(['rm', 'herwig_run.log', 'herwig_run.out', 'herwig_run.run', 'herwig_run.tex'])
@@ -26,41 +26,41 @@ def run_analyzer():
 		
 		call(['./bin/generateEvents', 'cards/pythia_card.cmnd', 'data/pythia_truth.hepmc'])
 
-		# call(['Herwig++', 'read', 'cards/herwig_card.in'])
-		# call(['Herwig++', 'run', 'herwig_run.run', '-d1'])
-		# call(['mv', 'herwig_run.hepmc', 'data/herwig_truth.hepmc'])	# Move to correct location.
+		call(['Herwig++', 'read', 'cards/herwig_card.in'])
+		call(['Herwig++', 'run', 'herwig_run.run', '-d1'])
+		call(['mv', 'herwig_run.hepmc', 'data/herwig_truth.hepmc'])	# Move to correct location.
 
-		# call(['Sherpa', '-f', 'cards/sherpa_card.dat'])
+		call(['Sherpa', '-f', 'cards/sherpa_card.dat'])
 
-		# call(['mv', 'sherpa_long_MPI.hepmc', 'data/sherpa_truth.hepmc'])	# Move to correct location.
+		call(['mv', 'sherpa_long_MPI.hepmc', 'data/sherpa_truth.hepmc'])	# Move to correct location.
 
 	def parse_hepmc():
 		call(['./bin/parseHepMC', 'data/pythia_truth.hepmc', 'data/pythia_truth.mod'])
-		# call(['./bin/parseHepMC', 'data/herwig_truth.hepmc', 'data/herwig_truth.mod'])
-		# call(['./bin/parseHepMC', 'data/sherpa_truth.hepmc', 'data/sherpa_truth.mod'])
+		call(['./bin/parseHepMC', 'data/herwig_truth.hepmc', 'data/herwig_truth.mod'])
+		call(['./bin/parseHepMC', 'data/sherpa_truth.hepmc', 'data/sherpa_truth.mod'])
 
 
 	def run_delphes():
 		call(['./bin/runDelphes', 'cards/delphes_card_CMS_NoFastJet.tcl', 'data/pythia_truth.hepmc'])
-		# call(['./bin/runDelphes', 'cards/delphes_card_CMS_NoFastJet.tcl', 'data/herwig_truth.hepmc'])
-		# call(['./bin/runDelphes', 'cards/delphes_card_CMS_NoFastJet.tcl', 'data/sherpa_truth.hepmc'])
+		call(['./bin/runDelphes', 'cards/delphes_card_CMS_NoFastJet.tcl', 'data/herwig_truth.hepmc'])
+		call(['./bin/runDelphes', 'cards/delphes_card_CMS_NoFastJet.tcl', 'data/sherpa_truth.hepmc'])
 
 		call(['mv', 'data/pythia_truth.hepmc.mod', 'data/pythia_reco.mod'])
-		# call(['mv', 'data/herwig_truth.hepmc.mod', 'data/herwig_reco.mod'])
-		# call(['mv', 'data/sherpa_truth.hepmc.mod', 'data/sherpa_reco.mod'])
+		call(['mv', 'data/herwig_truth.hepmc.mod', 'data/herwig_reco.mod'])
+		call(['mv', 'data/sherpa_truth.hepmc.mod', 'data/sherpa_reco.mod'])
 
 
 	# Delete all generated MC files plus "config" files. It's important to delete everything so that any change to a parameter in the MC-generation process actually trickles through.
-	# delete_everything()
+	delete_everything()
 	
 	# Run the three MC programs.
-	# run_mc_programs()
+	run_mc_programs()
 
 	# Parse the result HePMC files to MOD files.
 	parse_hepmc()
 
 	# Run Detector Simulation. We don't need to parse these because the simulation will write MOD files directly.
-	# run_delphes()
+	run_delphes()
 
 	cleanup()
 
